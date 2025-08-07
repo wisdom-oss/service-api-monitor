3797585d1ee11dc8798b2143f0d3f034ac5a4a3a
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-08-07 19:44:11 +0200

    chore: adding error handling into the application

--- types/v1/command.go
+++ types/v1/command.go
@@ -9,7 +9,7 @@ import (
 type Command struct {
 	Command string          `json:"command" validate:"required,gt=0"`
 	ID      string          `json:"id"`
-	Data    json.RawMessage `json:"data"    validate:"required"`
+	Data    json.RawMessage `json:"data"    validate:"omitempty,required"`
 }
 
 func (c Command) Validate() error {
--- types/v1/error.go
+++ types/v1/error.go
@@ -1,19 +1,7 @@
 package v1
 
-import "encoding/json"
-
 type CommandError struct {
-	IncomingMessageID any
-	Error             error
-	IncomingCommand   Command
-}
-
-func (ce CommandError) MarshalJSON() ([]byte, error) {
-	m := make(map[string]any)
-	m["type"] = "error"
-	m["relatedTo"] = ce.IncomingMessageID
-	m["error"] = ce.Error.Error()
-	m["receivedCommand"] = ce.IncomingCommand
-
-	return json.Marshal(m)
+	IncomingMessageID string `json:"relatedTo,omitempty"`
+	Error             string `json:"error"`
+	IncomingData      any    `json:"receivedData,omitempty"`
 }
